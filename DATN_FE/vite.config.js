fbf4881fa66095f5b5f962dba750d3095e3ab7e5
TrucTran2003 <ttruc386@gmail.com>
2025-05-09 12:15:45 +0700

    Update

--- DATN_FE/vite.config.js
+++ DATN_FE/vite.config.js
@@ -4,8 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    // server: {
-    //     host: "0.0.0.0", // Cho phép truy cập từ bên ngoài
-    //     port: 5173, // Hoặc cổng bạn muốn
-    // },
+    
 });
