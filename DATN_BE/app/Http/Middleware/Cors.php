b87faba5f4a8d82870a42ac1e6eb698a732694df
TrucTran2003 <ttruc386@gmail.com>
2025-05-20 11:36:56 +0700

    Add files via upload

---
+++ DATN_BE/app/Http/Middleware/Cors.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'http://172.20.10.3:5173') // Cho phép mọi domain (hoặc thay '*' bằng 'http://192.168.1.10:5173')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+    }
+}
